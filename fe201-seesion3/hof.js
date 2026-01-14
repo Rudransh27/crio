0389a811a7eb359ac0dc79166c1a614707e697fe
Rudransh27 <SSGM_306712@ssgmce.ac.in>
2026-01-15 01:01:39 +0530

    Initial commit

---
+++ fe201-seesion3/hof.js
@@ -0,0 +1,14 @@
+// higher order function
+// --> either returns another function
+// --> or accept another function as a argument
+
+function abc(){
+    return function def(){
+
+    }
+}
+
+let a=()=>{}
+function func(a,b){
+    a()
+}
